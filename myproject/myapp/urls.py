539b9578186ae7a7ca5c49c353913d103bfb24b2
safaa-hub <63284106+safaa-hub@users.noreply.github.com>
2020-04-19 11:47:09 +0300

    Delete urls.py

--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import RegisterViewSet, LoginView
-
-router = DefaultRouter()
-router.register(r'registration', viewset=RegisterViewSet, basename='user')
-router.register(r'login', viewset=LoginView, basename='user')
-
-urlpatterns = router.urls
