2276c57d0cf410c83225d5029d4da379295e1887
safaa-hub <63284106+safaa-hub@users.noreply.github.com>
2020-05-07 13:36:33 +0300

    allll

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework.routers import DefaultRouter
 from .views.login_view import LoginView
 from .views.register_view import RegisterViewSet
@@ -10,15 +9,3 @@ router.register(r'login', viewset=LoginView, basename='user')
 router.register(r'user', viewset=GetUser, basename='user')
 
 urlpatterns = router.urls
-=======
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import RegisterViewSet, LoginView, GetUser
-
-router = DefaultRouter()
-router.register(r'registration', viewset=RegisterViewSet, basename='user')
-router.register(r'login', viewset=LoginView, basename='user')
-router.register(r'user', viewset=GetUser, basename='user')
-
-urlpatterns = router.urls
->>>>>>> 002e0fea01e9e0adfba9b57106f62e2ad9467f5d
